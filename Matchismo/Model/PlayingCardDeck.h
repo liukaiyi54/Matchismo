c520d711f367bf6264f2f172928c0e6c3b37c109
Michael <liukaiyi54@gmail.com>
2015-09-17 09:33:17 +0800

    add PlayingCardDeck model

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Michael on 9/16/15.
+//  Copyright (c) 2015 Michael. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
