1890dc0431f3fc3ac4cb62892eb1327dc971d3af
Michael <liukaiyi54@gmail.com>
2015-09-18 23:27:15 +0800

    add more cards. add touch event.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
+#import "Deck.h"
 
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) NSInteger flipCount;
+@property (nonatomic, strong) Deck *deck;
 
 @end
 
@@ -20,11 +26,31 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
     }
+    
+    self.flipCount++;
+}
 
+- (void)setFlipCount:(NSInteger)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %ld", self.flipCount];
+}
+
+- (Deck *)deck {
+    if (!_deck) {
+        _deck = [self createDeck];
+    }
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
 }
 
 
